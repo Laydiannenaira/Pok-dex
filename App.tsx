93733c8fe8f953206eb5e7ae13e774b0adecbe33
Laydianne Costa <laydianneemilly@gmail.com>
2023-03-29 12:06:06 -0300

    Primeiro commit com toda aplicação finalizada

---
+++ App.tsx
@@ -0,0 +1,17 @@
+// import { StatusBar } from 'expo-status-bar';
+import { Text } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import theme from './src/global/styles/theme';
+import { Home } from './src/pages/Home';
+// import { Welcome } from './src/pages/Welcome';
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      {/* <Welcome /> */}
+      <Home />
+      <Text>Olá testando</Text>
+    </ThemeProvider>
+  );
+}
+
